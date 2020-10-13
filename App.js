fcc76dfc0c43a600c1f91084d0b6e7a122ebe590
Nassim55 <nassim.chenouf@outlook.com>
2020-10-13 09:19:12 +0100

    Viewshot working IOS and Android

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import { TextInput, Button } from 'react-native-paper'
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import ImagePicker from 'react-native-image-picker';
+import Animated from 'react-native-reanimated';
+import { usePanGestureHandler } from  "react-native-redash/lib/module/v1";
+import ViewShot from "react-native-view-shot";
 
 
 const App = () => {
